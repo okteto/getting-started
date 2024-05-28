dd52a142884aba48f1ce8643be89fd756f5aa7e1
Borja Gonzalez <borja@bgon.es>
2024-05-28 13:31:44 +0200

    add hero test

---
+++ e2e/tests/hero.spec.js
@@ -0,0 +1,20 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('displays the hero', async ({ page }) => {
+  await page.goto('/');
+
+  // Hero content
+  await expect(page.getByText('Bohemian Rhapsody')).toBeVisible();
+  await expect(page.getByText(
+    "Queen take the music world by storm when they form the rock 'n' roll band in 1970."
+  )).toBeVisible();
+
+  // Click the play button
+  await page.getByRole('link', { name: 'Play' }).click();
+  expect(page.url()).toMatch(/#$/); // TODO
+
+  // Click the list button
+  await page.getByRole('link', { name: 'My List' }).click();
+  expect(page.url()).toMatch(/#$/); // TODO
+});
