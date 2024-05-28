bfd0e68386f56b820fa8dde9f7ff4ec2ba222d20
Borja Gonzalez <borja@bgon.es>
2024-05-28 18:29:07 +0200

    add Item tests

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -6,7 +6,11 @@ export class Item extends Component {
   render() {
     return (
       <div className="Item">
-        <div className="ItemContainer" style={{ backgroundImage: `url(${this.props.backdrop})` }}>
+        <div
+          className="ItemContainer"
+          style={{ backgroundImage: `url(${this.props.backdrop})` }}
+          data-testid="item-container"
+        >
           <div className="overlay">
             <div className="title">{this.props.title}</div>
             <div className="rating">{this.props.score} / 10</div>
