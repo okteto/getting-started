4539b101cc44dcfae13731680d71bd76eb8ee2ca
Borja Gonzalez <borja@bgon.es>
2024-05-28 13:31:27 +0200

    add header test

---
+++ e2e/tests/header.spec.js
@@ -0,0 +1,19 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('displays the header', async ({ page }) => {
+  await page.goto('/');
+
+  // Logo
+  await expect(page.locator('.logo')).toBeVisible();
+
+  // Navigation
+  await expect(page.getByRole('list').getByText('Home')).toBeVisible();
+  await expect(page.getByRole('list').getByText('Movies')).toBeVisible();
+  await expect(page.getByRole('list').getByText('My List')).toBeVisible();
+
+  // Profile
+  await expect(page.getByText('Cindy Lopez')).toBeVisible();
+  const avatar = page.getByAltText('profile');
+  await expect(avatar).toHaveAttribute('src', /.+/);
+});
