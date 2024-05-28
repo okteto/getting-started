be78a86749ec3789db758f2995aad09350572020
Borja Gonzalez <borja@bgon.es>
2024-05-28 13:31:12 +0200

    add snapshot tests

---
+++ e2e/tests/main.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  // The page title
+  await expect(page).toHaveTitle('Movies');
+});
+
+test('matches screenshot', async ({ page }) => {
+  await page.goto('/');
+
+  // The page matches the screenshot
+  await expect(page).toHaveScreenshot('movies.png');
+});
